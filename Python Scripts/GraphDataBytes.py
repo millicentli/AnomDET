d6a56b79d17f997b0e75d1c6f674a23c10c3c723
Millicent Li <mllcntl@gmail.com>
2016-08-19 15:50:33 -0700

    Python graphing scripts

--- Python Scripts/GraphDataBytes.py
+++ Python Scripts/GraphDataBytes.py
@@ -6,15 +6,20 @@ import serial, csv, struct
 from time import localtime, strftime
 
 ser = serial.Serial('/dev/cu.LightBlue-Bean')
+#opens the file to be written in - make sure to change
 file = open('/Users/Millicent/Desktop/csvfile.csv', 'r+')
 writer = csv.writer(file)
+
+
 app = QtGui.QApplication([])
 win = pg.GraphicsWindow()
 win.showMaximized()
+#adds each plot name, title
 p1 = win.addPlot(title="X (Roll) vs No. of Samples")
 p2 = win.addPlot(title="Y (Pitch) vs No. of Samples")
 p3 = win.addPlot(title="Z (Yaw) vs No. of Samples")
 pg.setConfigOptions(antialias=True)
+#set the text labels
 p1.setLabel('left', text="X value")
 p1.setLabel('bottom', text="No. of Samples")
 p2.setLabel('left', text="Y value")
@@ -28,16 +33,16 @@ data1 = []
 data2 = []
 data3 = []
 
+
+#update function
 def update():
 	global curve, data, pos
 	if ser.readline():
-		#print strftime("%H:%M:%S", localtime())
-	#line = ser.readline()
-	#vals = line.split()
+		#takes the bytes and converts into a long int
 		x = struct.unpack_from('<L', ser.read(4))
 		y = struct.unpack_from('<L', ser.read(4))
 		z = struct.unpack_from('<L', ser.read(4))	
-	#writer.writerow([vals[0], vals[1], vals[2], strftime("%H:%M:%S", localtime())])
+		#writes to a file
 		writer.writerow([x[0], y[0], z[0], strftime("%H:%M:%S", localtime())])
 		a = float(x[0])
 		b = float(y[0])
@@ -45,6 +50,7 @@ def update():
 		data1.append(a)
 		data2.append(b)
 		data3.append(c)
+		#animates the graph
 		if len(data1 and data2 and data3) > 60:
 			data1[:] = data1[1:]
 			data2[:] = data2[1:]
@@ -54,10 +60,12 @@ def update():
 		curve3.setData(np.array(data3))
 		app.processEvents()
 	
-	
+
+#internal time which calls the update function
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(0)
+#default initializer for pyqtgraph
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
